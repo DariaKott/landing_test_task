112f538e970cd603adffea94ff7d953cabb60aa8
Daria Kot <poturayko@gmail.com>
2025-05-11 20:21:16 +0200

    added burger menu, finished the page

--- script.js
+++ script.js
@@ -6,3 +6,20 @@ window.addEventListener('scroll', function() {
       header.classList.remove('scrolled');
     }
   });
+
+const burger = document.querySelector('.burger');
+const menu = document.querySelector('.mobile-menu');
+
+burger.addEventListener('click', () => {
+  burger.classList.toggle('active');
+  menu.classList.toggle('active');
+});
+
+const menuLinks = document.querySelectorAll('.mobile-menu a');
+
+menuLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    burger.classList.remove('active');
+    menu.classList.remove('active');
+  });
+});
